ae07cf099151b4f4a2ecd317095afcddc187ddb4
mx <lmxwyl523@gmail.com>
2023-11-28 15:19:08 +0800

    Add files via upload

---
+++ banner/banner.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+
+def banner():
+    hound_art = """
+    \033[1;31m  / \\__
+     (    @\\___
+     /         O
+    /   (_____/  Author:dddinmx 
+    /_____/      Hound Detector\033[0m
+    """
+    print(hound_art)
