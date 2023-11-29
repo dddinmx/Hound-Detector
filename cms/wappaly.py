8deff7683e3d7766c1042197205abf788474dd85
mx <lmxwyl523@gmail.com>
2023-11-29 16:57:24 +0800

    Add files via upload

---
+++ cms/wappaly.py
@@ -0,0 +1,30 @@
+#coding:utf-8
+
+import datetime,sys
+from Wappalyzer import Wappalyzer, WebPage
+
+red =  "\033[1;31m"
+reset = "\033[0;0m"
+
+def wappaly(url):
+    try:
+        wappalyzer = Wappalyzer.latest()
+        webpage = WebPage.new_from_url(url)
+        apps = wappalyzer.analyze(webpage)
+        if apps:
+            hits = []
+            for app in apps:
+                hits.append(app)
+            b=(f"{', '.join(hits)}")
+            return b
+        else:
+            b = ""
+            return b
+    except Exception as e:
+        print ("["+checkwallytime()+"] "+red+"[+]"+reset+" Timeout.."+"\n")
+        pass
+    
+
+def checkwallytime():
+     time = datetime.datetime.now().strftime('%H:%M:%S')
+     return time
